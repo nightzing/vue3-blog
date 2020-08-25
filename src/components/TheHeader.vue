a694a88752d6370445dba4c3bfb6c82124fa8ccd
unknown <G006896@china-goldcard.com>
2020-08-25 17:08:57 +0800

    add: 头部组件

---
+++ src/components/TheHeader.vue
@@ -0,0 +1,30 @@
+<template>
+  <header class="header">
+    <div class="header__left">
+      <i class="header__icon header__icon--collapse" />
+    </div>
+    <div class="header__center">
+      <p class="header__title">干货集中营</p>
+    </div>
+    <RouterLink class="header__right" to="/user">
+      <i class="header__icon header__icon--search" />
+    </RouterLink>
+  </header>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "TheHeader",
+});
+</script>
+
+<style lang="scss" scoped>
+.header {
+  @include layout-flex(center, space-between);
+  @at-root #{&}__left {
+    text-align: left;
+  }
+}
+</style>
